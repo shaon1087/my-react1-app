7ee61eb2e238bf9e10992310060fc9c054acfb7e
shaon 1087 <shaonsaha1087@gmail.com>
2024-04-08 04:18:47 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Counter from './Counter';
 import Team from './Team';
 import User from './User';
+import Friends from './Friends';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
   return(
     <>
     <h1>react core concept</h1>
+    <Friends></Friends>
     <User></User>
     <Counter> 
 
---
+++ src/Friends.jsx
@@ -0,0 +1,27 @@
+
+import Friend from './Friend';
+import './Friends.css'
+import { useEffect, useState } from "react"
+
+
+export default function Friends(){
+
+    const [friends,setFriends] = useState([]);
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => setFriends(data))
+    }, []);
+
+
+    return(
+        <div className='box'>
+            <h3>Friends: {friends.length}</h3>
+            {
+                friends.map(friend => <Friend friend={friend}>
+                </Friend>)
+            }
+        </div>
+    )
+
+}
