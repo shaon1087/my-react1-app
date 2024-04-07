c3a4d4770128d996f50f2349051e02fc05878658
shaon 1087 <shaonsaha1087@gmail.com>
2024-04-08 03:12:49 +0600

    commit done

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,19 @@ function App() {
   function handleClick(){
     alert('button clicked');
   }
+
  const addToFive = (num) => {alert(num + 5)};
 
   return(
     <>
+    <h1>react core concept</h1>
     <Counter> 
 
    </Counter>
    <Team>
 
    </Team>
-    <h1>react core concept</h1>
+    
     <button onClick={()=> addToFive(5)}>Click me</button>
     </>
    )
